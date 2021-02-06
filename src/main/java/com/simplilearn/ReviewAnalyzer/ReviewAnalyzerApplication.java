a9a6532a270d7127b2512cd49eeedaced9cea9a8
vlhuang <vlhuang88@yahoo.com>
2021-02-06 20:02:51 +0000

    Addinf files

---
+++ src/main/java/com/simplilearn/ReviewAnalyzer/ReviewAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearn.ReviewAnalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewAnalyzerApplication.class, args);
+	}
+
+}
